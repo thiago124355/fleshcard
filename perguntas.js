a29387ebfd5136e6f275356d82fe90a9a33589a6
thiago124355 <00001112226023sp@al.educacao.sp.gov.br>
2024-11-04 12:17:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual a fóruma de Bhaskara?',
+    'Fórmula completa (-b±√(b²-4ac))/(2a)'
+)
+
+criaCartao(
+    'Técnologia e Robótica',
+    'Qual o comando que desfaz uma ação no VsCode?',
+    'Ctrl Z'
+)
+
+criaCartao(
+    'Empreendedorismo',
+    'Para criar uma proposta de valor forte, você deve identificar claramente o que?',
+    '1- O problema ou necessidade do cliente 2- A solução que você oferece 3- Os benefícios claros para o cliente'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
